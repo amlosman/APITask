c696680dd33154dff6ef03440cafb3c3c0a5837e
Amlosman <30425773+amlosman@users.noreply.github.com>
2022-04-07 11:56:42 +0200

    Update PatternSteps.java

--- API_UK_Task/src/test/java/StepDef/PatternSteps.java
+++ API_UK_Task/src/test/java/StepDef/PatternSteps.java
@@ -2,20 +2,13 @@ package StepDef;
 import ReastAssurtTest.PatternTest;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
 public class PatternSteps {
-    @Given("The APIs and GetActiveProductStatus MS base URL are ready")
-    public void theAPIsAndGetActiveProductStatusMSBaseURLAreReady() {
 
-    }
-    @When("The user calls Get Pattern PathParameter {string} successfully")
+   @Given("The user calls Get Pattern PathParameter {string} successfully")
     public void theUserCallsGetPatternPathParameterSuccessfully(String PathParameter) {
         PatternTest.getResponse(PathParameter);
         PatternTest.ValidateResponseStatus();
     }
-
-
     @Then("Check numViews {string} value is greater than {string}")
     public void checkNumViewsValueIsGreaterThan(String numViews, String numViewsValues) {
         PatternTest.ValidateNumView(numViews,numViewsValues );
